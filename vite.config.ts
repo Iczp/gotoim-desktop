ed4b124eb9b3ae29c2a905f86c890abad712f9db
zhongpei <1000@intry.cn>
2025-10-11 17:52:47 +0800

    0

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import path, { resolve } from 'path'
-import { ConfigEnv, defineConfig, loadEnv } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Unocss from '@unocss/vite'
 
 // Vite config specifically for the renderer (web) build
 
-{
-}
 export default defineConfig((args) => {
   const { mode } = args
   console.log('args', args)
