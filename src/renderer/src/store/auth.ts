02ba48403cd1d6ca73ef4ee56139ccdb4a347898
zhongpei <1000@intry.cn>
2025-10-13 17:31:58 +0800

    0

---
+++ src/renderer/src/store/auth.ts
@@ -0,0 +1,15 @@
+import { defineStore } from 'pinia'
+
+export const useAuthStore = defineStore(
+  'auth',
+  () => ({
+    state: () => {
+      return {}
+    },
+    getters: {},
+    actions: {},
+  }),
+  {},
+)
+
+export default useAuthStore
